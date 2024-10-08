65709fbff542db12460057728c6580639d05ccff
Peter4Tech <peter4tech@gmail.com>
2024-10-08 17:13:16 +0100

    Creating the login page and also the styling

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,7 +3,17 @@ import './login.css'
 
 function Login() {
     return(
-        <div> This is the Login page
+        <div className='login'> 
+        <div className="left">
+            <h2>Facebook Login</h2>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo magni ipsa expedita cum vero facere et quidem est, aliquam unde sint voluptatibus
+                 maxime obcaecati mollitia reprehenderit tenetur eveniet rerum! Dicta?</p>
+       <span>Don't Have an Account? </span>
+       <button className='btn btn-primary'> Register</button>
+        </div>
+        <div className="right">
+
+        </div>
 
         </div>
     )
