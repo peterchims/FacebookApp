de6e9e9871c350fb9434265914c5ecf4915c15d0
Peter4Tech <peter4tech@gmail.com>
2024-10-09 14:20:12 +0100

    Completed the styling of the sign up page

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -3,22 +3,28 @@ import './signup.css'
 
 function Signup() {
     return(
+        <>
+        
         <div className="Signup">
+        <h1 className='header'> Facebook</h1>
         <div className='card'> 
             <div className="signup-header">
-            <h1>  Create a new Account</h1>
+            <h1>  Create a new account</h1>
             <p> It's quick and easy.</p>
             </div>
-            <hr />
+            <hr className='borderLine' />
 
         <form className="right">
-<input type="text" name="" placeholder="First name" required id="" />
+            <div className="input-group">
+            <input type="text" name="" placeholder="First name" required id="" />
 <input type="text" name="" placeholder="Surname" required id="" />
 
-<div className="form-row">
+            </div>
+
+<div className="DOB">
             <label>Date of Birth:</label>
             <div className="dob-selection">
-              <select name="day" required>
+              <select name="day" className='month'  required>
                 <option value="">Day</option>
                 {Array.from({ length: 31 }, (_, i) => (
                   <option key={i + 1} value={i + 1}>
@@ -27,7 +33,7 @@ function Signup() {
                 ))}
               </select>
 
-              <select name="month" required>
+              <select name="month" className='month' required>
                 <option value="">Month</option>
                 {["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].map(
                   (month, i) => (
@@ -38,7 +44,7 @@ function Signup() {
                 )}
               </select>
 
-              <select name="year" required>
+              <select name="year" className='month' required>
                 <option value="">Year</option>
                 {Array.from({ length: 100 }, (_, i) => (
                   <option key={i} value={2024 - i}>
@@ -48,22 +54,33 @@ function Signup() {
               </select>
             </div>
           </div>
-<div className="form-row">
+<div className="gender">
             <label>Gender:</label>
+            <div className="gender-values">
             <label><input type="radio" name="gender" value="male" /> Male</label>
             <label><input type="radio" name="gender" value="female" /> Female</label>
             <label><input type="radio" name="gender" value="female" /> Custom</label>
+            </div>
           </div>
-<input type="email" name="" placeholder='Mobile number or Email Address' required id="" />
-<input type="email" name="" placeholder='New password' required id="" />
-        <p> People who use our service may have uploaded your contact information to Facebook. <a href="#">Learn more.</a></p>
-        <p>By clicking Sign Up, you agree to our <a href="#">Terms</a>,<a href="#">Privacy Policy</a>  and <a href="#">Cookies Policy</a> . You may receive SMS notifications from us and can opt out at any time.</p>
-       <button className='btn btn-create'> Sign Up</button>
-       <a href="#"> Already have an account?</a>
+          <div className="sensitive-data">
+          <input type="email" name="" placeholder='Mobile number or Email Address' required id="" />
+          <input type="password" name="" placeholder='New password' required id="" />
+          </div>
+          <div className="text-content">
+          <p className='p1'> People who use our service may have uploaded your contact information to Facebook. <a href="#">Learn more.</a></p>
+          <p className='p2'>By clicking Sign Up, you agree to our <a href="#">Terms</a>,<a href="#">Privacy Policy</a>  and <a href="#">Cookies Policy</a>. </p>
+          <p className='p3'>You may receive SMS notifications from us and can opt out at any time.</p>
+          </div>
+        <div className="signup-footer">
+        <button className='btn btn-create btn-sign'> Sign Up</button>
+        <a href="#" className='login-account'> Already have an account?</a>
+        </div>
+    
         </form>
 
         </div>
         </div>
+        </>
     )
 }
 
