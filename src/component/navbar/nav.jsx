dbd38afe9ab98306ab7b0c5c4254ccc325ee4553
Peter4Tech <peter4tech@gmail.com>
2024-10-11 15:53:21 +0100

    Completed the styling and the navigation section of my web application

--- src/component/navbar/nav.jsx
+++ src/component/navbar/nav.jsx
@@ -1,42 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './nav.css'
 import { Link } from 'react-router-dom'
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBars, faBell, faEnvelope, faHome, faSearch, faUser} from '@fortawesome/free-solid-svg-icons'
+// import { faFacebookMessenger } from '@fortawesome/fontawesome-svg-core';
+import {faBars, faBell,faHome, faSearch, faUsers, faGamepad, faPlayCircle, faStore, faMessage} from '@fortawesome/free-solid-svg-icons'
+import CurrentUser from '../../fackApes/CurrentUserData';
+import coverPhoto from '../../assets/icons/fb_logo.png'
 
 
 
 function Nav(){
+  const [isFocused, setIsFocused] = useState(false);
   return (
   <nav>
     <div className="nav-container">
+
+      {/* Left Navigation */}
       <div className="nav-left">
         <Link to='/'>
-        <h3 className='logo'> Facebook </h3>
-        </Link>
+       <img src={coverPhoto} alt="fb_logo" className='fb-logo' />
 
-        <Link to='/'>
-        <FontAwesomeIcon icon={faHome} />
-        </Link>
-        <Link to='/profile/id'>
-        <FontAwesomeIcon icon={faUser} />
         </Link>
         <div className="nav-searchbar">
-        <FontAwesomeIcon icon={faSearch} />
-        <input type="search" name="" id="" />
-        </div>
+      {/* Conditional rendering: only show the icon if the input field is not focused */}
+      {!isFocused && (
+        <FontAwesomeIcon className='fa-search' icon={faSearch} />
+      )}
+
+      {/* Input field */}
+      <input
+        type="search"
+        placeholder='Search here...'
+        onFocus={() => setIsFocused(true)}  // Hide the icon on focus
+        onBlur={() => setIsFocused(false)}   // Show the icon back on blur
+      />
+    </div>
       </div>
 
+<div className="nav-center">
+      
+
+        <Link to='/' className='center-link'>
+        <FontAwesomeIcon className="home-icon" icon={faHome} />
+        </Link>
+        <Link to='/' className='center-link'>
+        <FontAwesomeIcon className="icon" icon={faPlayCircle} />
+        </Link>
+        <Link to='/' className='center-link'>
+        <FontAwesomeIcon className="icon" icon={faStore} />
+        </Link>
+      
+        <Link to='/' className='center-link'>
+        <FontAwesomeIcon className="icon" icon={faUsers} />
+        </Link>
+        <Link to='/' className='center-link'>
+        <FontAwesomeIcon className="icon" icon={faGamepad} />
+        </Link>
+        
+        
+      </div>
+{/*  Right Navigation */}
       <div className="nav-right">
-        <Link to='/chatbox/id'>
-        <FontAwesomeIcon icon={faEnvelope} />
+        <Link to='/' className='navlink-right'>
+        <FontAwesomeIcon className='right-icon' id='navbar-bars' icon={faBars} />
         </Link>
-        <Link to='/'>
-        <FontAwesomeIcon icon={faBell} />
+        <Link to='/chatbox/id' className='navlink-right'>
+        <FontAwesomeIcon icon={faMessage} className='right-icon' />
+        </Link> 
+        <Link to='/' className='navlink-right'>
+        <FontAwesomeIcon icon={faBell} className='right-icon' />
         </Link>
-        <Link to='/'>
-        <FontAwesomeIcon icon={faBars} />
-        </Link>
+        <div className="user">
+          <img src={CurrentUser.map(user=>(user.ProfieImage))} alt="" />
+        </div>
       </div>
     </div>
   </nav>
--- src/fackApes/CurrentUserData.js
+++ src/fackApes/CurrentUserData.js
@@ -1,5 +1,6 @@
 import ProfieImage from '../assets/img/CurentProfile.jpeg';
 import CoverPhoto from '../assets/img/CoverPhotos.jpg';
+import fbLogo from '../assets/icons/fb_logo.png';
 
 const CurrentUser = [
     {
@@ -8,7 +9,9 @@ const CurrentUser = [
         username: '@peter4tech',
         ProfieImage: ProfieImage,
         CoverPhoto: CoverPhoto
-    }
+    },
+   
+
 ];
 
 export default CurrentUser;
